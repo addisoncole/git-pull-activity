9112ffb5f3c4ef23c388bad79477a9f43fa5ef27
Addison Cole <addison.cole85@gmail.com>
2018-08-15 11:16:20 -0700

    un commented new method

--- pull_practice.rb
+++ pull_practice.rb
@@ -12,9 +12,9 @@ def duck_noise
 end
 
 # Task 2: Same as task one, but switch roles
-# def truck_noise
-#   puts "honk"
-# end
+def truck_noise
+  puts "honk"
+end
 
 # Taks 3: Merging Changes
 # One partner should uncomment one method, and the other
@@ -25,14 +25,14 @@ end
 #   puts "beep boop"
 # end
 
-# def train_noise
-#   puts "choo choo"
-# end
+def train_noise
+  puts "choo choo"
+end
 
 # Task 4: Merge Conflict
 # Both partners should modify the same line in different ways.
 # Git doesn't know what to do in this case, so you'll have to
 # merge the two versions by hand.
-def clock_noise
-  puts ""
-end
+# def clock_noise
+#   puts ""
+# end
